43cae020f3988b56dafb312469e3a8684c86a00e
Cvansht <shivanshsandilya10@gmail.com>
2025-02-03 16:40:50 +0530

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Dashboard from "./pages/Dashboard";
 import { Signin } from "./pages/Signin";
 import {Signup }from "./pages/Signup";
-import { BrowserRouter ,Routes ,Route } from "react-router-dom";
+import { BrowserRouter ,Routes ,Route, Navigate , } from "react-router-dom";
 
 function App() {
   return (<BrowserRouter>
   <Routes>
+  <Route path="/" element={<Navigate to="/signup" replace />} />
     <Route path="/signup" element={<Signup/>}/>
     <Route path="/signin" element={<Signin/>}/>
     <Route path="/dashboard" element={<Dashboard/>}/>
